62f0542ece15bae24f85e3abf36061ffdf5e8101
juan-bol <juan-bol98@hotmail.com>
2022-07-14 14:34:32 -0500

    terraform done

--- example_terraform/main.tf
+++ example_terraform/main.tf
@@ -97,9 +97,24 @@ data "archive_file" "lambda_zip" {
 
 resource "aws_lambda_function" "saveHelloFunction" {
   filename         = data.archive_file.lambda_zip.output_path
-  function_name    = "lambda_terraform"
+  function_name    = "lambda_terraform_save"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "handler.saveHello"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  runtime          = "nodejs12.x"
+  environment {
+    variables = {
+      GREETINGS_TABLE = aws_dynamodb_table.greetingsTable.name
+    }
+  }
+}
+
+
+resource "aws_lambda_function" "getHelloFunction" {
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = "lambda_terraform_get"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "handler.getHello"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "nodejs12.x"
   environment {
@@ -127,7 +142,16 @@ resource "aws_api_gateway_method" "post_method" {
   api_key_required = false
 }
 
-resource "aws_api_gateway_integration" "integration" {
+resource "aws_api_gateway_method" "get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+  api_key_required = false
+}
+
+
+resource "aws_api_gateway_integration" "integration_post" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.resource.id
   http_method             = aws_api_gateway_method.post_method.http_method
@@ -136,19 +160,36 @@ resource "aws_api_gateway_integration" "integration" {
   uri                     = aws_lambda_function.saveHelloFunction.invoke_arn
 }
 
-resource "aws_lambda_permission" "apigw_lambda" {
+resource "aws_api_gateway_integration" "integration_get" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.get_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.getHelloFunction.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw_lambda_permission_post" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.saveHelloFunction.function_name
   principal     = "apigateway.amazonaws.com"
-
   source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.post_method.http_method}${aws_api_gateway_resource.resource.path}"
+}
+
+resource "aws_lambda_permission" "apigw_lambda_permission_get" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.getHelloFunction.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.get_method.http_method}${aws_api_gateway_resource.resource.path}"
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   depends_on = [
-    aws_api_gateway_integration.integration
+    aws_api_gateway_integration.integration_post,
+    aws_api_gateway_integration.integration_get
   ]
 }
 
